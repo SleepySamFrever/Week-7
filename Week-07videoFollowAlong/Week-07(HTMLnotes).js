74b3d7eb39df99152050dba83bec7e09c4269f79
Tyler Lambert <tslambert1919@gmail.com>
2022-09-17 12:12:19 -0600

    added week 7 progress

---
+++ Week-07videoFollowAlong/Week-07(HTMLnotes).js
@@ -0,0 +1,16 @@
+//html stands for hyper text markup languag
+//Used to give structure to website or application
+
+//To render html we use a web browser like chrome, firefox, etc to open an html file
+
+//html organizes content uses constructs called elements
+//Elements are the basic building blocks of html
+
+//An html element consists of tags, ususally an opening and closing tag
+//A tag starts with the < and ends with >
+
+//To comment in HTML we use <!--comments go here--!>
+
+
+
+
